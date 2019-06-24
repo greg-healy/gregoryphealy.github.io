e253ded804e92cb8a817a018d976d8b0e13a797d
Greg <greg.healy@pm.me>
2019-06-24 00:30:57 +0000

    Delete cat-facts.js

--- js/api/cat-facts.js
+++
@@ -1,7 +0,0 @@
-export function getCatFact() {
-  const baseUrl = 'https://cat-fact.herokuapp.com'
-  fetch(baseUrl + '/facts/random')
-    .then(response => response.json())
-    .then(response => resolve(response.text))
-    .catch(error => console.error('Error:', error))
-}
