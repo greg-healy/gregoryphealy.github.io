d20201c22648b332863aaf01324fb6a7950641e2
Greg <greg.healy@pm.me>
2019-06-24 00:31:01 +0000

    Delete history-facts.js

--- js/api/history-facts.js
+++
@@ -1,7 +0,0 @@
-export function getHistoryFact(){
-  const baseUrl = 'http://numbersapi.com';
-  fetch(baseUrl+'/random/year')
-    .then(response => response.json())
-    .then(response => resolve(response))
-    .catch(error => console.error('Error:',error))
-}
